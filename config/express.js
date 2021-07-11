e4c166bb713cf68c7c6c6d5b2c67bf4991ea5c30
Heedong Kim <ruthetum@gmail.com>
2021-07-11 18:04:40 +0900

    [feat] express 설정

---
+++ config/express.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const compression = require('compression');
+const methodOverride = require('method-override');
+var cors = require('cors');
+module.exports = function () {
+    const app = express();
+    app.set("view engine", "ejs");
+    app.set("views", process.cwd() + "/views");
+
+    app.use(compression());
+    app.use(express.json());
+    app.use(express.urlencoded({extended: true}));
+    app.use(methodOverride());
+    app.use(cors());
+    app.use(express.static(process.cwd() + '/public'));
+
+    // Routing configure
+    require('../src/router')(app);
+    
+    return app;
+};
