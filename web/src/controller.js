b0d10377768152f6d53f48ab4ae7a0c678841d46
Serin <bcode429@gmail.com>
2021-08-04 20:55:02 +0900

    [feat] 모니터링 화면 UI

--- web/src/controller.js
+++ web/src/controller.js
@@ -110,8 +110,24 @@ exports.patient = async function (req, res) {
         return res.redirect('/');
     }
 
-    return res.render('clinic.ejs', {heartRate, temperature, oxygen});
+    return res.render('clinic.ejs', {patientIdx, heartRate, temperature, oxygen});
 };
+
+exports.patientMonitor = async function (req, res) {
+    const patientIdx = parseInt(req.params.patientIdx, 10);
+    const authUser = parseInt(req.verifiedToken.id, 10);
+    const heartRate = 68; // 심박동수 예시
+    const temperature = 36.5; // 체온 예시
+    const oxygen = 98; // 산소포화도 예시 
+
+    // 잘못된 접근 - 환자 인덱스와 토큰의 인덱스가 다를 때
+    if (patientIdx !== authUser) {
+        logger.info(`Error Patient - patientIdx and token.id are different`);
+        return res.redirect('/');
+    }
+
+    return res.render('patientMonitor.ejs', {heartRate, temperature, oxygen});
+}
 
 exports.doctor = async function (req, res) {
     const doctorIdx = parseInt(req.params.doctorIdx, 10);
