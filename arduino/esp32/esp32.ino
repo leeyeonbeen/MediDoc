ba4865b39e8d8d879c051ea17a0206edea51b388
yeonbeen <lyb0228@kpu.ac.kr>
2021-08-09 23:06:38 +0900

    esp32 JSON code

--- arduino/esp32/esp32.ino
+++ arduino/esp32/esp32.ino
@@ -1,17 +1,22 @@
 #include<WiFi.h>
+#include "ArduinoJson.h"
 #include<AWS_IOT.h>
 
 #define WIFI_SSID "{wifi_ssid}"
 #define WIFI_PASSWD "{wifi_passwd}"
 
 #define CLIENT_ID "{client_id}"
-#define MQTT_TOPIC "{mqtt_topic}"
+#define MQTT_TOPIC "{mqtt_id}"
 #define AWS_HOST "{aws_host}"
+
+#define RXp2 16
+#define TXp2 17
 
 AWS_IOT aws;
 
 void setup(){
-  Serial.begin(9600);
+  Serial.begin(115200);
+  Serial2.begin(9600, SERIAL_8N1, RXp2, TXp2);
   Serial.print("\ninitializing thing temp_humidity_\n");
 
   Serial.print("\n initialing wifi: connecting to ");
@@ -24,9 +29,6 @@ void setup(){
   } 
 Serial.println("\n connected.\n done");
 
-Serial.print("\n initialing DHT11...");
-Serial.println("Done.");
-
 Serial.println("\n initialing connection to AWS...");
 if(aws.connect(AWS_HOST,CLIENT_ID)==0){
   Serial.println(" connected to AWS\n done.");
@@ -38,12 +40,17 @@ Serial.println("done\n\ndone.\n");
 }
 
 void loop(){
-    String temp_humidity="temp = ";
-    temp_humidity += "success ";
-  
-    String message="Welcome";
-    char payload[40];
-    temp_humidity.toCharArray(payload,40);
+    String temp_humidity= "";
+    StaticJsonBuffer<200> jsonBuffer;
+    JsonObject& root = jsonBuffer.createObject();
+    root["format"] = "string";
+    root["topic"] = MQTT_TOPIC;
+    root["timestamp"] = "1627363020648";
+    root["payload"] = Serial2.readString();
+    root.printTo(temp_humidity);
+    
+    char payload[200];
+    temp_humidity.toCharArray(payload,200);
 
     Serial.println("pub : ");
   Serial.println(payload);
@@ -53,5 +60,4 @@ void loop(){
   else{
     Serial.println("failed\n");
   }
-  delay(1000);
 }
