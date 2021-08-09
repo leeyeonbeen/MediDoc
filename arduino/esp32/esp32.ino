e7c9425e2a01f91c78d6316778550572cceb3b20
yeonbeen <lyb0228@kpu.ac.kr>
2021-08-09 23:11:24 +0900

    add timestamp code

--- arduino/esp32/esp32.ino
+++ arduino/esp32/esp32.ino
@@ -1,18 +1,21 @@
+#include <TimeLib.h>
+#include <timestamp32bits.h>
 #include<WiFi.h>
-#include "ArduinoJson.h"
+#include <ArduinoJson.h>
 #include<AWS_IOT.h>
 
 #define WIFI_SSID "{wifi_ssid}"
 #define WIFI_PASSWD "{wifi_passwd}"
 
 #define CLIENT_ID "{client_id}"
-#define MQTT_TOPIC "{mqtt_id}"
+#define MQTT_TOPIC "{mqtt_topic}"
 #define AWS_HOST "{aws_host}"
 
 #define RXp2 16
 #define TXp2 17
 
 AWS_IOT aws;
+timestamp32bits stamp = timestamp32bits(2000);
 
 void setup(){
   Serial.begin(115200);
@@ -25,39 +28,42 @@ void setup(){
   Serial.print(" ");
   while(WiFi.status()!=WL_CONNECTED){
    Serial.print(".");
-  delay(500);
+   delay(500);
   } 
-Serial.println("\n connected.\n done");
-
-Serial.println("\n initialing connection to AWS...");
-if(aws.connect(AWS_HOST,CLIENT_ID)==0){
-  Serial.println(" connected to AWS\n done.");
-}
-else{
- Serial.println("connection failed\n make syre blabla"); 
-}
-Serial.println("done\n\ndone.\n");
+  Serial.println("\n connected.\n done");
+  Serial.println("\n initialing connection to AWS...");
+  if(aws.connect(AWS_HOST,CLIENT_ID)==0){
+    Serial.println(" connected to AWS\n done.");
+  }
+  else{
+    Serial.println("connection failed\n make syre blabla"); 
+  }
+  Serial.println("done\n\ndone.\n");
 }
 
 void loop(){
-    String temp_humidity= "";
-    StaticJsonBuffer<200> jsonBuffer;
+  String serial2 = Serial2.readString();
+  if(serial2 != ""){
+    String jsondata= "";
+    StaticJsonBuffer<300> jsonBuffer;
     JsonObject& root = jsonBuffer.createObject();
-    root["format"] = "string";
+    root["format"] = "json";
     root["topic"] = MQTT_TOPIC;
-    root["timestamp"] = "1627363020648";
-    root["payload"] = Serial2.readString();
-    root.printTo(temp_humidity);
+    root["timestamp"] = stamp.timestamp(year(),month(),day(),hour(),minute(),second());
+    root["payload"] = serial2;
+    root.printTo(jsondata);
     
-    char payload[200];
-    temp_humidity.toCharArray(payload,200);
+    char payload[300];
+    jsondata.toCharArray(payload,300);
 
     Serial.println("pub : ");
-  Serial.println(payload);
-  if(aws.publish(MQTT_TOPIC,payload)==0){
-    Serial.println("success\n");    
-  }
-  else{
-    Serial.println("failed\n");
+    Serial.println(payload);
+
+    if(aws.publish(MQTT_TOPIC,payload)==0){
+      Serial.println("success\n");    
+    }
+    else{
+      Serial.println("failed\n"); 
+    }
   }
 }
