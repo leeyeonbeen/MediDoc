d0a2c9f58ff0cd1700c4f52ef5da9caa0a4ad365
rmflsdkqlqn <hello.git.rmflsdkqlqn@naver.com>
2021-08-13 15:18:42 +0900

    심박,산소포화도+ oled+ 버튼합체시도

--- arduino/arduino_sensor/sensor.ino
+++ arduino/arduino_sensor/sensor.ino
@@ -1,124 +1,319 @@
-//연빈,max30102심박, 산소포화도 시리얼 모니터 확인용 코드
-#include <Wire.h>
-#include "MAX30105.h"
-#include "spo2_algorithm.h"
+//승연, 심박,산소포화도+ oled+ 버튼합체시도
+#include "ssd1306h.h"
+#include "MAX30102.h"
+#include "Pulse.h"
+#include <avr/pgmspace.h>
+#include <EEPROM.h>
+#include <avr/sleep.h>
 
-MAX30105 particleSensor;
-
-#define MAX_BRIGHTNESS 255
-
-#if defined(__AVR_ATmega328P__) || defined(__AVR_ATmega168__)
-//Arduino Uno doesn't have enough SRAM to store 100 samples of IR led data and red led data in 32-bit format
-//To solve this problem, 16-bit MSB of the sampled data will be truncated. Samples become 16-bit data.
-uint16_t irBuffer[100]; //infrared LED sensor data
-uint16_t redBuffer[100];  //red LED sensor data
-#else
-uint32_t irBuffer[100]; //infrared LED sensor data
-uint32_t redBuffer[100];  //red LED sensor data
+// Routines to clear and set bits 
+#ifndef cbi
+#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
+#endif
+#ifndef sbi
+#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
 #endif
 
-int32_t bufferLength; //data length
-int32_t spo2; //SPO2 value
-int8_t validSPO2; //indicator to show if the SPO2 calculation is valid
-int32_t heartRate; //heart rate value
-int8_t validHeartRate; //indicator to show if the heart rate calculation is valid
 
-byte pulseLED = 11; //Must be on PWM pin
-byte readLED = 13; //Blinks with each data read
+SSD1306 oled; 
+MAX30102 sensor;
+Pulse pulseIR;
+Pulse pulseRed;
+MAFilter bpm;
 
-void setup()
-{
-  Serial.begin(115200); // initialize serial communication at 115200 bits per second:
+#define LED LED_BUILTIN
+#define BUTTON 3
+#define OPTIONS 7
 
-  pinMode(pulseLED, OUTPUT);
-  pinMode(readLED, OUTPUT);
+static const uint8_t heart_bits[] PROGMEM = { 0x00, 0x00, 0x38, 0x38, 0x7c, 0x7c, 0xfe, 0xfe, 0xfe, 0xff, 
+                                        0xfe, 0xff, 0xfc, 0x7f, 0xf8, 0x3f, 0xf0, 0x1f, 0xe0, 0x0f,
+                                        0xc0, 0x07, 0x80, 0x03, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 
+                                        0x00, 0x00 };
 
-  // Initialize sensor
-  if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed
-  {
-    Serial.println(F("MAX30105 was not found. Please check wiring/power."));
-    while (1);
-  }
+//spo2_table is approximated as  -45.060*ratioAverage* ratioAverage + 30.354 *ratioAverage + 94.845 ;
+const uint8_t spo2_table[184] PROGMEM =
+        { 95, 95, 95, 96, 96, 96, 97, 97, 97, 97, 97, 98, 98, 98, 98, 98, 99, 99, 99, 99, 
+          99, 99, 99, 99, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 
+          100, 100, 100, 100, 99, 99, 99, 99, 99, 99, 99, 99, 98, 98, 98, 98, 98, 98, 97, 97, 
+          97, 97, 96, 96, 96, 96, 95, 95, 95, 94, 94, 94, 93, 93, 93, 92, 92, 92, 91, 91, 
+          90, 90, 89, 89, 89, 88, 88, 87, 87, 86, 86, 85, 85, 84, 84, 83, 82, 82, 81, 81, 
+          80, 80, 79, 78, 78, 77, 76, 76, 75, 74, 74, 73, 72, 72, 71, 70, 69, 69, 68, 67, 
+          66, 66, 65, 64, 63, 62, 62, 61, 60, 59, 58, 57, 56, 56, 55, 54, 53, 52, 51, 50, 
+          49, 48, 47, 46, 45, 44, 43, 42, 41, 40, 39, 38, 37, 36, 35, 34, 33, 31, 30, 29, 
+          28, 27, 26, 25, 23, 22, 21, 20, 19, 17, 16, 15, 14, 12, 11, 10, 9, 7, 6, 5, 
+          3, 2, 1 } ;
 
-  Serial.println(F("Attach sensor to finger with rubber band. Press any key to start conversion"));
-  while (Serial.available() == 0) ; //wait until user presses a key
-  Serial.read();
 
-  byte ledBrightness = 60; //Options: 0=Off to 255=50mA
-  byte sampleAverage = 4; //Options: 1, 2, 4, 8, 16, 32
-  byte ledMode = 2; //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
-  byte sampleRate = 100; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
-  int pulseWidth = 411; //Options: 69, 118, 215, 411
-  int adcRange = 4096; //Options: 2048, 4096, 8192, 16384
-
-  particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange); //Configure sensor with these settings
+int getVCC() {
+  //reads internal 1V1 reference against VCC
+  #if defined(__AVR_ATmega1284P__)
+    ADMUX = _BV(REFS0) | _BV(MUX4) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);  // For ATmega1284
+  #else
+    ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);  // For ATmega328
+  #endif
+  delay(2); // Wait for Vref to settle
+  ADCSRA |= _BV(ADSC); // Convert
+  while (bit_is_set(ADCSRA, ADSC));
+  uint8_t low = ADCL;
+  unsigned int val = (ADCH << 8) | low;
+  //discard previous result
+  ADCSRA |= _BV(ADSC); // Convert
+  while (bit_is_set(ADCSRA, ADSC));
+  low = ADCL;
+  val = (ADCH << 8) | low;
+  
+  return (((long)1024 * 1100) / val)/100;  
 }
 
-void loop()
-{
-  bufferLength = 100; //buffer length of 100 stores 4 seconds of samples running at 25sps
+void print_digit(int x, int y, long val, char c=' ', uint8_t field = 3,const int BIG = 2)
+    {  
+    uint8_t ff = field;
+    do { 
+        char ch = (val!=0) ? val%10+'0': c;
+        oled.drawChar( x+BIG*(ff-1)*6, y, ch, BIG);
+        val = val/10; 
+        --ff;
+    } while (ff>0);
+}
 
-  //read the first 100 samples, and determine the signal range
-  for (byte i = 0 ; i < bufferLength ; i++)
-  {
-    while (particleSensor.available() == false) //do we have new data?
-      particleSensor.check(); //Check the sensor for new data
+const uint8_t MAXWAVE = 72;
 
-    redBuffer[i] = particleSensor.getRed();
-    irBuffer[i] = particleSensor.getIR();
-    particleSensor.nextSample(); //We're finished with this sample so move to next sample
+class Waveform {
+  public:
+    Waveform(void) {wavep = 0;}
 
-    Serial.print(F("red="));
-    Serial.print(redBuffer[i], DEC);
-    Serial.print(F(", ir="));
-    Serial.println(irBuffer[i], DEC);
-  }
+      void record(int waveval) {
+        waveval = waveval/8;         
+        waveval += 128;              //shift so entired waveform is +ve  
+        waveval = waveval<0? 0 : waveval;
+        waveform[wavep] = (uint8_t) (waveval>255)?255:waveval; 
+        wavep = (wavep+1) % MAXWAVE;
+      }
+  
+      void scale() {
+        uint8_t maxw = 0;
+        uint8_t minw = 255;
+        for (int i=0; i<MAXWAVE; i++) { 
+          maxw = waveform[i]>maxw?waveform[i]:maxw;
+          minw = waveform[i]<minw?waveform[i]:minw;
+        }
+        uint8_t scale8 = (maxw-minw)/4 + 1;  //scale * 8 to preserve precision
+        uint8_t index = wavep;
+        for (int i=0; i<MAXWAVE; i++) {
+          disp_wave[i] = 31-((uint16_t)(waveform[index]-minw)*8)/scale8;
+          index = (index + 1) % MAXWAVE;
+        }
+      }
 
-  //calculate heart rate and SpO2 after first 100 samples (first 4 seconds of samples)
-  maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
+void draw(uint8_t X) {
+  for (int i=0; i<MAXWAVE; i++) {
+    uint8_t y = disp_wave[i];
+    oled.drawPixel(X+i, y);
+    if (i<MAXWAVE-1) {
+      uint8_t nexty = disp_wave[i+1];
+      if (nexty>y) {
+        for (uint8_t iy = y+1; iy<nexty; ++iy)  
+        oled.drawPixel(X+i, iy);
+        } 
+        else if (nexty<y) {
+          for (uint8_t iy = nexty+1; iy<y; ++iy)  
+          oled.drawPixel(X+i, iy);
+          }
+       }
+    } 
+}
 
-  //Continuously taking samples from MAX30102.  Heart rate and SpO2 are calculated every 1 second
-  while (1)
-  {
-    //dumping the first 25 sets of samples in the memory and shift the last 75 sets of samples to the top
-    for (byte i = 25; i < 100; i++)
-    {
-      redBuffer[i - 25] = redBuffer[i];
-      irBuffer[i - 25] = irBuffer[i];
+private:
+    uint8_t waveform[MAXWAVE];
+    uint8_t disp_wave[MAXWAVE];
+    uint8_t wavep = 0;
+    
+} wave;
+
+int  beatAvg;
+int  SPO2, SPO2f;
+int  voltage;
+bool filter_for_graph = false;
+bool draw_Red = false;
+uint8_t pcflag =0;
+uint8_t istate = 0;
+uint8_t sleep_counter = 0;
+
+void button(void){
+    pcflag = 1;
+}
+
+void checkbutton(){
+    if (pcflag && !digitalRead(BUTTON)) {
+      istate = (istate +1) % 4;
+      filter_for_graph = istate & 0x01;
+      draw_Red = istate & 0x02;
+      EEPROM.write(OPTIONS, filter_for_graph);
+      EEPROM.write(OPTIONS+1, draw_Red);
     }
+    pcflag = 0;
+}
 
-    //take 25 sets of samples before calculating the heart rate.
-    for (byte i = 75; i < 100; i++)
-    {
-      while (particleSensor.available() == false) //do we have new data?
-        particleSensor.check(); //Check the sensor for new data
+void Display_5(){
+   if(pcflag && !digitalRead(BUTTON)){
+     draw_oled(5);
+     delay(1100);
+   }
+   pcflag = 0;
+ 
 
-      digitalWrite(readLED, !digitalRead(readLED)); //Blink onboard LED with every data read
+}
 
-      redBuffer[i] = particleSensor.getRed();
-      irBuffer[i] = particleSensor.getIR();
-      particleSensor.nextSample(); //We're finished with this sample so move to next sample
+void go_sleep() {
+    oled.fill(0);
+    oled.off();
+    delay(10);
+    sensor.off();
+    delay(10);
+    cbi(ADCSRA, ADEN);  // disable adc
+    delay(10);
+    pinMode(0,INPUT);
+    pinMode(2,INPUT);
+    set_sleep_mode(SLEEP_MODE_PWR_DOWN);     
+    sleep_mode();  // sleep until button press 
+    // cause reset
+    setup();
+}
 
-      //send samples and calculation result to terminal program through UART
-      Serial.print(F("red="));
-      Serial.print(redBuffer[i], DEC);
-      Serial.print(F(", ir="));
-      Serial.print(irBuffer[i], DEC);
+void draw_oled(int msg) {
+    oled.firstPage();
+    do{
+    switch(msg){
+        case 0:  oled.drawStr(10,0,F("Device error"),1); 
+                 break;
+        case 1:  oled.drawStr(13,10,F("PLACE"),1); 
+                 oled.drawStr(10,20,F("FINGER"),1);
+                 oled.drawChar(100,0,voltage/10+'0');
+                 oled.drawChar(106,0,'.');
+                 oled.drawChar(112,0,voltage%10+'0');
+                 oled.drawChar(118,0,'V');
+                 oled.drawStr(84,14,F("Display"),1); 
+                 if (draw_Red) 
+                    oled.drawStr(84,24,F("Red"),1); 
+                 else
+                    oled.drawStr(84,24,F("IR"),1); 
+                 if (filter_for_graph) 
+                    oled.drawStr(108,24,F("Avg"),1); 
+                 else
+                    oled.drawStr(108,24,F("Raw"),1); 
+                 break;
+        case 2:  print_digit(86,0,beatAvg);
+                 wave.draw(8);
+                 print_digit(98,16,SPO2f,' ',3,1);
+                 oled.drawChar(116,16,'%');
+                 print_digit(98,24,SPO2,' ',3,1);
+                 oled.drawChar(116,24,'%');
+                 break;
+        case 3:  oled.drawStr(30,9,F("Thanks for"),1);
+                 oled.drawStr(30,20,F("testing"),1);
+                 //oled.drawXBMP(6,8,16,16,heart_bits);
+                
+                 break;
+        case 4:  oled.drawStr(28,12,F("OFF IN"),1);
+                 oled.drawChar(76,12,10-sleep_counter/10+'0');
+                 oled.drawChar(82,12,'s');
+                 break;
+        case 5:  oled.drawStr(0,0,F("BMP:"),1); 
+                 print_digit(25,0,beatAvg);
+                 oled.drawStr(0,15,F("SpO2:"),1); 
+                 print_digit(25,15,SPO2);
+                 oled.drawXBMP(106,8,16,16,heart_bits);
+                 break;
+        }
+    } while (oled.nextPage());
+}
 
-      Serial.print(F(", HR="));
-      Serial.print(heartRate, DEC);
-
-      Serial.print(F(", HRvalid="));
-      Serial.print(validHeartRate, DEC);
-
-      Serial.print(F(", SPO2="));
-      Serial.print(spo2, DEC);
-
-      Serial.print(F(", SPO2Valid="));
-      Serial.println(validSPO2, DEC);
-    }
-
-    //After gathering 25 new samples recalculate HR and SP02
-    maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
+void setup(void) {
+  pinMode(LED, OUTPUT);
+  pinMode(BUTTON, INPUT_PULLUP);
+  filter_for_graph = EEPROM.read(OPTIONS);
+  draw_Red = EEPROM.read(OPTIONS+1);
+  oled.init();
+  oled.fill(0x00);
+  draw_oled(3);
+  delay(3000); 
+  if (!sensor.begin())  {
+    draw_oled(0);
+    while (1);
   }
+  sensor.setup(); 
+  attachInterrupt(digitalPinToInterrupt(BUTTON),button, CHANGE);
+}
+
+long lastBeat = 0;    //Time of the last beat 
+long displaytime = 0; //Time of the last display update
+bool led_on = false;
+
+
+void loop()  {
+    sensor.check();
+    long now = millis();   //start time of this cycle
+    if (!sensor.available()) return;
+    uint32_t irValue = sensor.getIR(); 
+    uint32_t redValue = sensor.getRed();
+    sensor.nextSample();
+    if (irValue<5000) {
+        voltage = getVCC();
+        checkbutton();
+        draw_oled(sleep_counter<=50 ? 1 : 4); // finger not down message
+        delay(200);
+        ++sleep_counter;
+        if (sleep_counter>100) {
+          go_sleep(); 
+          sleep_counter = 0;
+        }
+    } else {
+        sleep_counter = 0;
+        int16_t IR_signal, Red_signal;
+        bool beatRed, beatIR;
+        if (!filter_for_graph) {
+           IR_signal =  pulseIR.dc_filter(irValue) ;
+           Red_signal = pulseRed.dc_filter(redValue);
+           beatRed = pulseRed.isBeat(pulseRed.ma_filter(Red_signal));
+           beatIR =  pulseIR.isBeat(pulseIR.ma_filter(IR_signal));        
+        } else {
+           IR_signal =  pulseIR.ma_filter(pulseIR.dc_filter(irValue)) ;
+           Red_signal = pulseRed.ma_filter(pulseRed.dc_filter(redValue));
+           beatRed = pulseRed.isBeat(Red_signal);
+           beatIR =  pulseIR.isBeat(IR_signal);
+        }
+        // invert waveform to get classical BP waveshape
+        wave.record(draw_Red ? -Red_signal : -IR_signal ); 
+        // check IR or Red for heartbeat     
+        if (draw_Red ? beatRed : beatIR){
+            long btpm = 60000/(now - lastBeat);
+            if (btpm > 0 && btpm < 200) beatAvg = bpm.filter((int16_t)btpm);
+            lastBeat = now; 
+            digitalWrite(LED, HIGH); 
+            led_on = true;
+            // compute SpO2 ratio
+            long numerator   = (pulseRed.avgAC() * pulseIR.avgDC())/256;
+            long denominator = (pulseRed.avgDC() * pulseIR.avgAC())/256;
+            int RX100 = (denominator>0) ? (numerator * 100)/denominator : 999;
+            // using formula
+            SPO2f = (10400 - RX100*17+50)/100;  
+            // from table
+            if ((RX100>=0) && (RX100<184))
+              SPO2 = pgm_read_byte_near(&spo2_table[RX100]);
+        }
+        // update display every 50 ms if fingerdown
+        if (now-displaytime>50) {
+            displaytime = now;
+            wave.scale();
+            draw_oled(2);
+            
+        }
+        Display_5();
+ 
+    }
+    // flash led for 25 ms
+    if (led_on && (now - lastBeat)>25){
+        digitalWrite(LED, LOW);
+        led_on = false;
+     }
 }
