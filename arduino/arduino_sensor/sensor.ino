5c652e0095f75c1cede3d6d99b3491cc6e339b22
rmflsdkqlqn <hello.git.rmflsdkqlqn@naver.com>
2021-08-13 14:39:01 +0900

    MAX30102 새코드+시리얼모니터출력

--- arduino/arduino_sensor/sensor.ino
+++ arduino/arduino_sensor/sensor.ino
@@ -1,52 +1,74 @@
-//승연, AWS값 송출을 위한 MAX30102 새로운 코드 시도
-#include <Wire.h>
-#include "MAX30100_PulseOximeter.h"
+//주은, 새로운 코드 시도,max30102+시리얼 모니터에 값 출력
+#include"WiFi.h"
+#include"DHT.h"
+#include"AWS_IOT.h"
 
-#define REPORTING_PERIOD_MS     1000
+#define DHT_PIN 33
+#define DHT_TYPE DHT11
 
-PulseOximeter pox;    //  맥박, 산소포화도 관련 객체 생성
+#define WIFI_SSID "TWOSOME_2.4G"
+#define WIFI_PASSWD "0327154467"
 
-uint32_t tsLastReport = 0;
+#define CLIENT_ID "lily"
+#define MQTT_TOPIC "$aws/things/lily/shadow/lily/update"
+#define AWS_HOST "a2utwh13lgm1sf-ats.iot.us-east-2.amazonaws.com"
 
-// 콜백함수, 맥박이 감지되면 실행될 함수
-void onBeatDetected()
-{
-    Serial.println("Beat!");
+DHT dht(DHT_PIN,DHT_TYPE);
+AWS_IOT aws;
+
+void setup(){
+  Serial.begin(9600);
+  Serial.print("\ninitializing thing temp_humidity_\n");
+
+  Serial.print("\n initialing wifi: connecting to ");
+  Serial.println(WIFI_SSID);
+  WiFi.begin(WIFI_SSID,WIFI_PASSWD);
+  Serial.print(" ");
+  while(WiFi.status()!=WL_CONNECTED){
+   Serial.print(".");
+  delay(500);
+  }
+  
+Serial.println("\n connected.\n done");
+Serial.print("\n initialing DHT11...");
+dht.begin();
+Serial.println("Done.");
+
+Serial.println("\n initialing connection to AWS...");
+
+if(aws.connect(AWS_HOST,CLIENT_ID)==0){
+  Serial.println(" connected to AWS\n done.");
+}
+else{
+ Serial.println("connection failed\n make syre blabla"); 
+}
+Serial.println("done\n\ndone.\n");
 }
 
-void setup()
-{
-    Serial.begin(115200);
+void loop(){
+  float temp=dht.readTemperature();
 
-    Serial.print("Initializing pulse oximeter..");
+  if(temp==NAN){
+    Serial.println("reading failed");
+  }
+  else{
+    String temp_humidity="temp = ";
+    temp = temp * 5.0 / 10.24;
+    temp_humidity +=String(temp);
+    temp_humidity ="C ";
+  
+    String message="Welcome";
+    char payload[40];
+    temp_humidity.toCharArray(payload,40);
 
-    // 맥박, 산소포화도 관련 객체 초기화
-
-    if (!pox.begin()) {
-        Serial.println("FAILED");
-        for(;;);
-    } else {
-        Serial.println("SUCCESS");
-    }
-
-   
-    // 콜백함수 등록
-    pox.setOnBeatDetectedCallback(onBeatDetected);
-}
-
-void loop()
-{
-    // 센서값을 계속 최신화
-    pox.update();
-
-    // 1초에 한번씩 시리얼 모니터에 값을 출력
-    if (millis() - tsLastReport > REPORTING_PERIOD_MS) {
-        Serial.print("Heart rate:");
-        Serial.print(pox.getHeartRate());
-        Serial.print("bpm / SpO2:");
-        Serial.print(pox.getSpO2());
-        Serial.println("%");
-
-        tsLastReport = millis();
-    }
+    Serial.println("pub : ");
+  Serial.println(payload);
+  if(aws.publish(MQTT_TOPIC,payload)==0){
+    Serial.println("success\n");    
+  }
+  else{
+    Serial.println("failed\n");
+  }
+  }
+  delay(1000);
 }
