bcdb57b03e02356f98fddb60ca3918462603895f
rmflsdkqlqn <hello.git.rmflsdkqlqn@naver.com>
2021-08-13 15:41:58 +0900

    온도빼고 esp32, aws연결 코드 합친거

--- arduino/arduino_sensor/sensor.ino
+++ arduino/arduino_sensor/sensor.ino
@@ -1,20 +1,14 @@
-//주은, 온도센서 다른 코드 + AWS 연결+ 제대로 된 값 출력 시도
+//연빈,온도빼고 esp32, aws연결 코드 합친거 
 #include<WiFi.h>
-#include<DHT.h>
 #include<AWS_IOT.h>
-#include <LM35.h>
 
-#define DHT_PIN 33
-#define DHT_TYPE DHT11
-
-#define WIFI_SSID "U"
+#define WIFI_SSID "U5"
 #define WIFI_PASSWD "4"
 
 #define CLIENT_ID "l"
 #define MQTT_TOPIC "e"
 #define AWS_HOST "m"
 
-DHT dht(DHT_PIN,DHT_TYPE);
 AWS_IOT aws;
 
 void setup(){
@@ -32,7 +26,6 @@ void setup(){
 Serial.println("\n connected.\n done");
 
 Serial.print("\n initialing DHT11...");
-dht.begin();
 Serial.println("Done.");
 
 Serial.println("\n initialing connection to AWS...");
@@ -46,14 +39,8 @@ Serial.println("done\n\ndone.\n");
 }
 
 void loop(){
-  float temp=dht.readTemperature();
-
-  if(temp==NAN){
-    Serial.println("reading failed");
-  }
-  else{
+  
     String temp_humidity="temp = ";
-    temp_humidity +=String(temp);
     temp_humidity ="success ";
   
     String message="Welcome";
@@ -68,6 +55,6 @@ void loop(){
   else{
     Serial.println("failed\n");
   }
-  }
+  
   delay(1000);
 }
